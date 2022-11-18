2642a26f1b6195be3c03a6941d93c48996a8d0e3
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2022-11-18 22:19:13 +0300

    Create project

---
+++ Queue2/Queue2/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Queue2
+//
+//  Created by Ваня Науменко on 18.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
